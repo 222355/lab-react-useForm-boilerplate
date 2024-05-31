461397af15a98764d50e7877952967920edd35cc
Akshara <aksharar02@gmail.com>
2024-05-31 17:15:45 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./App.css";
+import FormComponent from "./Component/FormComponent";
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <FormComponent />
+      </div>
+    );
+  }
+}
